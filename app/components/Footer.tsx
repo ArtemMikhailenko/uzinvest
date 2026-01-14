6f4010cc351dd3f58d3900e7604fb14713464a3f
ArtemMikhailenko <Artemlend@gmail.com>
2026-01-14 15:31:09 +0200

    edits photo and address

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -2,7 +2,7 @@
 
 export default function Footer() {
   return (
-    <footer className="relative py-8 px-4 border-t border-white/10 bg-white/20 backdrop-blur-md  mt-12">
+    <footer className="relative py-8 px-4 border-t border-white/10   mt-12">
       <div className="max-w-7xl mx-auto">
         <div className="flex flex-col md:flex-row items-center justify-between gap-4">
           {/* Логотип и название */}
@@ -17,7 +17,7 @@ export default function Footer() {
           {/* Копирайт */}
           <div className="text-center">
             {/* <p className="text-white/70 text-sm">© UzInvest, 2026</p> */}
-            <p className="text-black/60 text-xs">Все права защищены</p>
+            {/* <p className="text-black/60 text-xs">Все права защищены</p> */}
           </div>
 
           {/* Контакты */}
